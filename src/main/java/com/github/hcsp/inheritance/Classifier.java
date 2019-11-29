4a089a43061d12362e98d35035ad3dd590c4efe6
silvia-YQY <545067444@qq.com>
2019-11-29 23:19:09 +0800

    将对象按照类型分类 (#79)
    将对象按照类型分类 (#79)

--- src/main/java/com/github/hcsp/inheritance/Classifier.java
+++ src/main/java/com/github/hcsp/inheritance/Classifier.java
@@ -31,5 +31,15 @@ public class Classifier {
             List<Object> list,
             List<Number> numberList,
             List<String> stringList,
-            List<Object> otherList) {}
+            List<Object> otherList) {
+        list.forEach(item->{
+            if(item instanceof Number){
+                numberList.add((Number) item);
+            }else if(item instanceof String){
+                stringList.add((String) item);
+            }else{
+                otherList.add(item);
+            }
+        });
+    }
 }
