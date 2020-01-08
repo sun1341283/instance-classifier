4d4a5540d0038adac72c812d67fb7f9018410e47
spclock <58385369+spclock@users.noreply.github.com>
2020-01-08 11:49:14 +0800

    将对象按照类型分类 (#104)
    将对象按照类型分类 (#104)

--- src/main/java/com/github/hcsp/inheritance/Classifier.java
+++ src/main/java/com/github/hcsp/inheritance/Classifier.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Classifier {
+
+
     public static void main(String[] args) {
         List<Object> list = Arrays.asList("0", 1, 2L, "3", new Object());
         List<Number> numbers = new ArrayList<>();
@@ -22,14 +24,36 @@ public class Classifier {
      * 给定一个包含任意对象的列表，将其按照以下方式分类： 如果对象是Number类型，将其放入numberList； 如果对象是String类型，将其放入stringList；
      * 否则，将其放入otherList。
      *
-     * @param list 给定的包含任意对象的列表
+     * @param list       给定的包含任意对象的列表
      * @param numberList 用于接收所有Number对象的列表
      * @param stringList 用于接收所有String对象的列表
-     * @param otherList 用于接收其余所有类型对象的列表
+     * @param otherList  用于接收其余所有类型对象的列表
      */
     public static void classify(
             List<Object> list,
             List<Number> numberList,
             List<String> stringList,
-            List<Object> otherList) {}
+            List<Object> otherList) {
+        for(Object obj:list){
+            if (obj instanceof Number) {
+                numberList.add((Number) obj);
+            } else if (obj instanceof String) {
+                stringList.add((String) obj);
+            } else {
+                otherList.add(obj);
+            }
+        }
+//        for(int i = 0;i<list.size();i++){
+//            if (list.get(i) instanceof Number) {
+//                numberList.add((Number) list.get(i));
+//            } else if (list.get(i) instanceof String) {
+//                stringList.add((String) list.get(i));
+//            } else {
+//                otherList.add(list.get(i));
+//            }
+//        }
+    }
+
 }
+
+
