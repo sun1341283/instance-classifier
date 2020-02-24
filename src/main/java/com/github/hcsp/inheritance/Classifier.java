f967abda14cd55752c49d8b2193bf078dcd13831
walkerxin <44486634+walkerxin@users.noreply.github.com>
2020-02-25 00:14:06 +0800

    classify different type of classes (#124)
    classify different type of classes (#124)

--- src/main/java/com/github/hcsp/inheritance/Classifier.java
+++ src/main/java/com/github/hcsp/inheritance/Classifier.java
@@ -22,14 +22,25 @@ public class Classifier {
      * 给定一个包含任意对象的列表，将其按照以下方式分类： 如果对象是Number类型，将其放入numberList； 如果对象是String类型，将其放入stringList；
      * 否则，将其放入otherList。
      *
-     * @param list 给定的包含任意对象的列表
+     * @param list       给定的包含任意对象的列表
      * @param numberList 用于接收所有Number对象的列表
      * @param stringList 用于接收所有String对象的列表
-     * @param otherList 用于接收其余所有类型对象的列表
+     * @param otherList  用于接收其余所有类型对象的列表
      */
     public static void classify(
             List<Object> list,
             List<Number> numberList,
             List<String> stringList,
-            List<Object> otherList) {}
+            List<Object> otherList) {
+        for (Object obj :
+                list) {
+            if (obj instanceof Number) {
+                numberList.add((Number) obj);
+            } else if (obj instanceof String) {
+                stringList.add((String) obj);
+            } else {
+                otherList.add(obj);
+            }
+        }
+    }
 }
