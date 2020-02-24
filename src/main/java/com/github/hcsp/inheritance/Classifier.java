6bcacc6536044bc58d1b0dc90b251559f9cc5efd
Leo Feng <553931681@qq.com>
2020-02-24 15:23:50 +0800

    将对象按照类型分类 (#122)
    将对象按照类型分类 (#122)

--- src/main/java/com/github/hcsp/inheritance/Classifier.java
+++ src/main/java/com/github/hcsp/inheritance/Classifier.java
@@ -12,7 +12,6 @@ public class Classifier {
         List<Object> others = new ArrayList<>();
 
         classify(list, numbers, strings, others);
-
         System.out.println("numbers = " + numbers);
         System.out.println("strings = " + strings);
         System.out.println("others = " + others);
@@ -31,5 +30,16 @@ public class Classifier {
             List<Object> list,
             List<Number> numberList,
             List<String> stringList,
-            List<Object> otherList) {}
+            List<Object> otherList) {
+
+        for (Object obj : list) {
+            if (obj instanceof Number) {
+                numberList.add((Number) obj);
+            } else if (obj instanceof String) {
+                stringList.add((String) obj);
+            } else {
+                otherList.add(obj);
+            }
+        }
+    }
 }
